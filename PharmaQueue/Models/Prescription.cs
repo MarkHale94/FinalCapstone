8641e31f798d3549fc2be33fb22027556bdef547
Mark Hale <mark.hale94@gmail.com>
2018-12-11 15:14:18 -0600

    created some basic models

---
+++ PharmaQueue/Models/Prescription.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmaQueue.Models
+{
+    public class Prescription
+    {
+        [Required]
+        public int PrescriptionId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Strength { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public int Refills { get; set; }
+
+        [Required]
+        public double price { get; set; }
+
+        [Required]
+        public bool isSold { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+    }
+}
---
+++ PharmaQueue/Models/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmaQueue.Models
+{
+    public class Status
+    {
+        [Required]
+        public int StatusId { get; set; }
+
+        [Required]
+        public string QueueStatus { get; set; }
+    }
+}
