df6d855acf79f0451a99aae7aac8885cf2002310
Mark Hale <mark.hale94@gmail.com>
2018-12-12 11:09:34 -0600

    fixed a bug that prevented the startup page or the login page from loading correctly

---
+++ PharmaQueue/Controllers/PrescriptionsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PharmaQueue.Data;
+using PharmaQueue.Models;
+
+namespace PharmaQueue.Controllers
+{
+    public class PrescriptionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PrescriptionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Prescriptions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Prescription.Include(p => p.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Prescriptions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prescription = await _context.Prescription
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.PrescriptionId == id);
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+
+            return View(prescription);
+        }
+
+        // GET: Prescriptions/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Prescriptions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PrescriptionId,Name,Strength,Quantity,Refills,Price,IsSold,UserId,PrescriptionStatusId,DateCreated")] Prescription prescription)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(prescription);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", prescription.UserId);
+            return View(prescription);
+        }
+
+        // GET: Prescriptions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prescription = await _context.Prescription.FindAsync(id);
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", prescription.UserId);
+            return View(prescription);
+        }
+
+        // POST: Prescriptions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PrescriptionId,Name,Strength,Quantity,Refills,Price,IsSold,UserId,PrescriptionStatusId,DateCreated")] Prescription prescription)
+        {
+            if (id != prescription.PrescriptionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(prescription);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PrescriptionExists(prescription.PrescriptionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", prescription.UserId);
+            return View(prescription);
+        }
+
+        // GET: Prescriptions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prescription = await _context.Prescription
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.PrescriptionId == id);
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+
+            return View(prescription);
+        }
+
+        // POST: Prescriptions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var prescription = await _context.Prescription.FindAsync(id);
+            _context.Prescription.Remove(prescription);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PrescriptionExists(int id)
+        {
+            return _context.Prescription.Any(e => e.PrescriptionId == id);
+        }
+    }
+}
