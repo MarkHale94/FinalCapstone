1f5b64bb7b63020ea6a4f04276af1fdfc445fa4e
Mark Hale <mark.hale94@gmail.com>
2018-12-13 11:03:40 -0600

    added a search feature that searches by the customer's last name and allows you to correctly link up to the details for that customer.

--- PharmaQueue/Controllers/CustomersController.cs
+++ PharmaQueue/Controllers/CustomersController.cs
@@ -58,67 +58,27 @@ namespace PharmaQueue.Controllers
             return View();
         }
 
-        // POST: Customers/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+        // GET: Search Products
+        [Authorize]
+        public async Task<IActionResult> Search(string search)
+        {
+            CustomerSearchViewModel viewModel = new CustomerSearchViewModel();
+
+            viewModel.Search = search;
+            var Customers = await _context.Users.Where(u=>u.LastName.Contains(search)).ToListAsync();
+            List<Customer> customerList = new List<Customer>();
+            foreach (var customer in Customers)
             {
-                return View();
+                Customer customerToAdd = new Customer();
+                customerToAdd.UserId = customer.Id;
+                customerToAdd.FirstName = customer.FirstName;
+                customerToAdd.LastName = customer.LastName;
+                customerList.Add(customerToAdd);
             }
+            viewModel.Customers = customerList;
+            return View(viewModel);
         }
 
-        // GET: Customers/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Customers/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Customers/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Customers/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
