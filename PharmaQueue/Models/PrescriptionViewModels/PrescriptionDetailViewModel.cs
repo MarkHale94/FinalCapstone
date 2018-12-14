499db6b583097574fb8f508fbcbf1864ef9909d2
unknown <mark.hale94@gmail.com>
2018-12-13 21:21:50 -0600

    fixed an issue where all users could see edit and delete buttons for prescriptions even if they weren't of the Employee UserType. Fixed Minor View issues with buttons on sold prescription details

---
+++ PharmaQueue/Models/PrescriptionViewModels/PrescriptionDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmaQueue.Models.PrescriptionViewModels
+{
+    public class PrescriptionDetailViewModel
+    {
+        public int CurrentUserTypeId { get; set; }
+        public Prescription Prescription { get; set; }
+    }
+}
