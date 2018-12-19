eb6b7e5cb1e95a71ea0026cadd42637e2c743a81
Mark Hale <mark.hale94@gmail.com>
2018-12-19 17:53:26 -0600

    page no longer has to reload in order for the new information to show up. now it sends an xml request that replaces the innerHTML of the body div

--- PharmaQueue/wwwroot/js/status.js
+++ PharmaQueue/wwwroot/js/status.js
@@ -7,5 +7,22 @@ connection.start().catch(function (err) {
 });
 
 connection.on("PrescriptionUpdate", function () {
-    window.location.reload();
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                document.getElementById("bodyForStuff").innerHTML = xmlhttp.responseText;
+            }
+            else if (xmlhttp.status == 400) {
+                alert('There was an error 400');
+            }
+            else {
+                console.log(xmlhttp.response)
+                alert('something else other than 200 was returned');
+            }
+        }
+    };
+    xmlhttp.open("GET", "https://localhost:5001/", true);
+    xmlhttp.send();
 });
