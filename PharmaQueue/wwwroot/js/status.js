cb9d45e46acf72c2efdc0329e02d99ae536a5e3b
unknown <mark.hale94@gmail.com>
2019-01-06 10:26:13 -0600

    updated the xml request to utilize the fetch api instead

--- PharmaQueue/wwwroot/js/status.js
+++ PharmaQueue/wwwroot/js/status.js
@@ -9,23 +9,9 @@ connection.start().catch(function (err) {
 connection.on("PrescriptionUpdate", function (userId) {
     var hiddenId = document.getElementById("hiddenId").value;
     if (hiddenId == userId) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-                if (xmlhttp.status == 200) {
-                    document.getElementById("bodyForStuff").innerHTML = xmlhttp.responseText;
-                }
-                else if (xmlhttp.status == 400) {
-                    alert('There was an error 400');
-                }
-                else {
-                    console.log(xmlhttp.response)
-                    alert('something else other than 200 was returned');
-                }
-            }
-        };
-        xmlhttp.open("GET", "https://localhost:5001/", true);
-        xmlhttp.send();
+        return fetch('https://localhost:5001/')
+            .then(result => result.text())
+            .then(result => document.getElementById("bodyForStuff").innerHTML = result)
     } else {
         return;
     }
